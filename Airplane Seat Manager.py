09cbb5a7d4e1e701d3383cedb6b42a7edd84efbb
QuintsGuy <taknott@email.neit.edu>
2023-01-31 00:16:58 -0500

    Week 4 Lab

--- Airplane Seat Manager.py
+++ Airplane Seat Manager.py
@@ -26,11 +26,31 @@ def get_seat():
         return None
     return (row, col)
 
+def assign_another():
+  while True:
+    check_again=input("\nDo you want to assign anymore seats? Y/N: ").upper()
+    try:
+      check_again=str(check_again)
+    except ValueError:
+      print("Invalid input -- try again\n")  
+    if check_again=="Y":
+      return 1
+    elif check_again=="N":
+      return False
+    else:
+      print("Invalid input -- try again")
+
 display_seats()
 while len(assigned_seats) < 28:
     seat = get_seat()
     if seat:
         if assign_seat(*seat):
             display_seats()
+        returned_option=assign_another()
+        if returned_option==1:
+            continue
+        else:
+            print('Goodbye...\n')
+            break
     else:
         print("All seats filled.")
