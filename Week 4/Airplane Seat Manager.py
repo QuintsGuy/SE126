68217bc0706768966ecea76fbfb3aca024a4f05d
QuintsGuy <taknott@email.neit.edu>
2023-01-31 08:44:54 -0500

    Airplane Seat Manager

--- Week 4/Airplane Seat Manager.py
+++ Week 4/Airplane Seat Manager.py
@@ -13,16 +13,18 @@ def assign_seat(row, col):
     if seats[row][col] == "X":
         print("Seat already occupied.")
         return False
-    seats[row][col] = "X"
-    assigned_seats.append((row, col))
-    return True
+    else:
+      seats[row][col] = "X"
+      assigned_seats.append((row, col))
+      return True
 
+#needs work to return invalid statement when inputing enteries outside range
 def get_seat():
-    seat = input("Enter seat (e.g. 1A): ")
+    seat = input("\nEnter seat (e.g. 1A): ") 
     row = int(seat[0]) - 1
     col = ord(seat[1]) - 65
-    if row>6 or col>3:
-        print("Invalid seat.")
+    if 0>row>6 or 0>col>3:
+        print("Invalid seat -- try again\n")
         return None
     return (row, col)
 
@@ -34,23 +36,22 @@ def assign_another():
     except ValueError:
       print("Invalid input -- try again\n")  
     if check_again=="Y":
-      return 1
+      return True
     elif check_again=="N":
+      print("Goodbye...\n")
       return False
     else:
       print("Invalid input -- try again")
 
 display_seats()
 while len(assigned_seats) < 28:
-    seat = get_seat()
-    if seat:
-        if assign_seat(*seat):
-            display_seats()
-        returned_option=assign_another()
-        if returned_option==1:
-            continue
-        else:
-            print('Goodbye...\n')
-            break
-    else:
-        print("All seats filled.")
+  seat = get_seat()
+  if seat == None:
+    continue
+  else:  
+    if assign_seat(*seat):
+      display_seats()
+      if assign_another():
+        continue
+      else:
+        break
