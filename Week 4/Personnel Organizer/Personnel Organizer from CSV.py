95412c451267c774f2eb5f4f7b168020e36e7bac
QuintsGuy <taknott@email.neit.edu>
2023-02-01 18:57:28 -0500

    Week 4 Labs

--- Week 4/Personnel Organizer/Personnel Organizer from CSV.py
+++ Week 4/Personnel Organizer/Personnel Organizer from CSV.py
@@ -29,20 +29,18 @@ def get_file(csv_file):
 
         print(f"\nNumber of personnel records processed: {personnel}")
 
-def find_another():
-  while True:
-    check_again=input("\nDo you want to lookup anyone else? Y/N: ").upper()
-    try:
-      check_again=str(check_again)
-    except ValueError:
-      print("Invalid input -- try again\n")  
-    if check_again=="Y":
-      return True
-    elif check_again=="N":
-      print("Goodbye...\n")
-      return False
-    else:
-      print("Invalid input -- try again")
+def prompt_locator():
+    check_person=input("\nIs there someone you would like to find? Y/N: ").upper()
+    while check_person==str(check_person):
+        if check_person=="Y":
+            return True
+        elif check_person=="N":
+            print("Goodbye...\n")
+            return False
+        else:
+            print("Invalid input -- try again")
+    print("Invalid input -- try again\n")  
+        
 
 first_name = []
 last_name = []
@@ -54,7 +52,7 @@ position = []
 csv_file = str(input(r"Please enter a file path: "))
 get_file(csv_file)
 
-while True:
+while prompt_locator()==True:
     find_person(first_name, last_name, phone_number, email_address, department, position)
-    if find_another() == False:
+    if prompt_locator() == False:
         break
