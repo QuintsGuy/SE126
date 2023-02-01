f9788f6bf66b0b057898c3b065d02fac34f7439c
QuintsGuy <taknott@email.neit.edu>
2023-02-01 08:52:46 -0500

    Week 4 Labs

--- Week 4/Personnel Organizer/Personnel Organizer from CSV.py
+++ Week 4/Personnel Organizer/Personnel Organizer from CSV.py
@@ -1,10 +1,10 @@
 import csv
 
 def find_person(first_name, last_name, phone_number, email_address, department, position):
-    look_up = input("Enter a person's last name: ")
+    look_up = input("Enter a person's last name: ").lower()
     for i in range(0, len(first_name)):
-        if look_up == last_name[i]:
-            print(f"Found it! The person you're looking for is:")
+        if look_up == last_name[i].lower():
+            print(f"\nFound it! The person you're looking for is:")
             print('{:<15}{:<15}{:<20}{:<30}{:<20}{:<15}'.format('Last', 'First', 'Phone', 'Email', 'Department', 'Position'))
             print('{:<15}{:<15}{:<20}{:<30}{:<20}{:<15}'.format(last_name[i], first_name[i], phone_number[i], email_address[i], department[i], position[i]))
 
@@ -29,6 +29,21 @@ def get_file(csv_file):
 
         print(f"\nNumber of personnel records processed: {personnel}")
 
+def find_another():
+  while True:
+    check_again=input("\nDo you want to lookup anyone else? Y/N: ").upper()
+    try:
+      check_again=str(check_again)
+    except ValueError:
+      print("Invalid input -- try again\n")  
+    if check_again=="Y":
+      return True
+    elif check_again=="N":
+      print("Goodbye...\n")
+      return False
+    else:
+      print("Invalid input -- try again")
+
 first_name = []
 last_name = []
 phone_number = []
@@ -39,4 +54,7 @@ position = []
 csv_file = str(input(r"Please enter a file path: "))
 get_file(csv_file)
 
-find_person(first_name, last_name, phone_number, email_address, department, position)
+while True:
+    find_person(first_name, last_name, phone_number, email_address, department, position)
+    if find_another() == False:
+        break
